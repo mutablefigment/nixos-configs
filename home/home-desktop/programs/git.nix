3983bbbfe5de0bbcff30c317a3837cdc2ed447f5
Mutable <gh@mutablefigment.xyz>
2025-06-10 22:39:49 +0200

    Added jujutsu with config options

--- home/home-desktop/programs/git.nix
+++ home/home-desktop/programs/git.nix
@@ -1,19 +1,40 @@
 {
   pkgs,
   ...
-}: {
+}: 
+let
+  name = "Mutable";
+  email = "gh@mutablefigment.xyz";
+  signkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOU+iWnGXqMpNj5wZOXN7/IpEz7Cavarzs2rM7RS/aq";
+in 
+{
   home.packages = [ 
     pkgs.gh 
   ];
 
   programs.git = {
     enable = true;
-    userName = "Mutable";
-    userEmail = "gh@mutablefigment.xyz";
+    userName = name;
+    userEmail = email;
     extraConfig = {
       commit.gpgsign = true;
       gpg.format = "ssh";
-      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOU+iWnGXqMpNj5wZOXN7/IpEz7Cavarzs2rM7RS/aq";
+      user.signingkey = signkey;
+    };
+  };
+
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      user = {
+        name = name;
+        email = email;
+      };
+      signing = {
+        behavior = "own";
+        backend = "ssh";
+        key = signkey;
+      };
     };
   };
 }
